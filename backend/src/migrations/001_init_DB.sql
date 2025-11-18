35e0e549a2ef1c529e2d3e5434f3ca1d4cb5d585
arnob-bro <abdulmohaimenkhanarnob@gmail.com>
2025-11-19 02:43:48 +0600

    backend: init database

--- backend/src/migrations/001_init_DB.sql
+++ backend/src/migrations/001_init_DB.sql
@@ -2,71 +2,29 @@
 
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
--- 1. Roles
-CREATE TABLE roles (
-  id BIGSERIAL PRIMARY KEY,
-  name VARCHAR(20) UNIQUE NOT NULL -- 'admin', 'manager'
-);
 
--- 2. Permissions
-CREATE TABLE permissions (
-  id BIGSERIAL PRIMARY KEY,
-  code VARCHAR(50) UNIQUE NOT NULL,
-  description TEXT
-);
-
--- 3. Role ↔ Permissions
-CREATE TABLE role_permissions (
-  role_id BIGINT NOT NULL REFERENCES roles(id) ON DELETE CASCADE,
-  permission_id BIGINT NOT NULL REFERENCES permissions(id) ON DELETE CASCADE,
-  PRIMARY KEY (role_id, permission_id)
-);
-
--- 4. Users (custom ID as VARCHAR)
 CREATE TABLE users (
-    user_id VARCHAR(20) PRIMARY KEY,
+    user_id UUID PRIMARY KEY,
     email VARCHAR(150) UNIQUE NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
     role VARCHAR(20) NOT NULL DEFAULT 'customer', ---(customer,employee)
-    verified BOOLEAN DEFAULT FALSE,
-    new_pass_token TEXT UNIQUE,
+    first_name VARCHAR(150) NOT NULL,
+    last_name VARCHAR(150) NOT NULL,
+    phone VARCHAR(20),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE employees (
-    employee_id VARCHAR(50) PRIMARY KEY,
-    first_name VARCHAR(100) NOT NULL,
-    last_name VARCHAR(100) NOT NULL,
-    email VARCHAR(150) UNIQUE NOT NULL REFERENCES users(email) ON UPDATE CASCADE,
-    phone VARCHAR(50) NOT NULL,
-    position VARCHAR(100) NOT NULL,
-    role_id BIGINT NOT NULL REFERENCES roles(id),
-    hire_date DATE NOT NULL,
-    address TEXT,
+
+CREATE TABLE user_addresses (
+    user_address_id UUID PRIMARY KEY,
+    phone VARCHAR(20) UNIQUE NOT NULL,
+    address_line_1 VARCHAR(255) NOT NULL,
+    address_line_2 VARCHAR(255) NOT NULL,
     city VARCHAR(100),
     country VARCHAR(100),
-    status VARCHAR(20) NOT NULL DEFAULT 'active' CHECK (
-        status IN ('active', 'on_leave', 'terminated', 'probation')
-    ),
-    avatar TEXT,
-    emergency_contact JSONB,  -- { "name": "...", "relationship": "...", "phone": "..." }
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-
-
--- 5. Services
-CREATE TABLE services (
-    service_id BIGSERIAL PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    short_desc TEXT NOT NULL,
-    key_benefits JSONB NOT NULL, -- store array of strings
-    our_process JSONB NOT NULL,  -- store array of strings
-    technologies JSONB NOT NULL DEFAULT '[]',
-    active BOOLEAN DEFAULT TRUE,
-    icon VARCHAR(100),           -- optional, can store "FaCode" etc.
+    postal_code VARCHAR(10),
+    is_default BOOLEAN DEFAULT FALSE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
@@ -74,172 +32,100 @@ CREATE TABLE services (
 
 -- 6. Categories
 CREATE TABLE categories (
-    category_id BIGSERIAL PRIMARY KEY,
-    category_name VARCHAR(150) UNIQUE NOT NULL,
+    category_id UUID PRIMARY KEY,
+    name VARCHAR(150) UNIQUE NOT NULL,
+    slug VARCHAR(150),
+    parent_id UUID NOT NULL REFERENCES categories(category_id) ON DELETE CASCADE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- -- 7. Projects (custom ID as VARCHAR)
--- CREATE TABLE projects (
---     project_id VARCHAR(20) PRIMARY KEY, -- e.g., "PRJ001"
---     title VARCHAR(150) NOT NULL,
---     overview TEXT,
---     case_study TEXT,
---     technologies TEXT[] DEFAULT '{}',
---     image_url VARCHAR(255),
---     github_url VARCHAR(255),
---     live_url VARCHAR(255),
---     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
--- );
+CREATE TABLE products (
+    product_id UUID PRIMARY KEY,
+    name VARCHAR(150) UNIQUE NOT NULL,
+    slug VARCHAR(150),
+    description TEXT,
+    price FLOAT NOT NULL,
+    discount_price FLOAT NOT NULL,
+    is_active BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
--- -- 8. Project ↔ Categories
--- CREATE TABLE project_categories (
---     project_id VARCHAR(20) NOT NULL REFERENCES projects(project_id) ON DELETE CASCADE,
---     category_id BIGINT NOT NULL REFERENCES categories(category_id) ON DELETE CASCADE,
---     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
---     PRIMARY KEY (project_id, category_id)
--- );
+CREATE TABLE product_variants (
+    product_variant_id UUID PRIMARY KEY,
+    size VARCHAR(5) NOT NULL,
+    color VARCHAR(20),
+    sku VARCHAR(255),
+    product_id UUID NOT NULL REFERENCES products(product_id) ON DELETE CASCADE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
+CREATE TABLE product_images (
+    product_image_id UUID PRIMARY KEY,
+    image_url TEXT NOT NULL,
+    is_featured BOOLEAN DEFAULT FALSE,
+    product_id UUID NOT NULL REFERENCES products(product_id) ON DELETE CASCADE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
+CREATE TABLE product_reviews (
+    product_review_id UUID PRIMARY KEY,
+    rating FLOAT NOT NULL,
+    comment TEXT,
+    user_id UUID NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
+    product_id UUID NOT NULL REFERENCES products(product_id) ON DELETE CASCADE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
-CREATE TABLE portfolio_items (
-    portfolio_item_id SERIAL PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    category VARCHAR(255) NOT NULL,
-    description TEXT NOT NULL,
-    image TEXT,
-    problem TEXT NOT NULL,
-    solution TEXT NOT NULL,
-    results TEXT NOT NULL,
-    tech_stack JSONB NOT NULL,
-    active BOOLEAN DEFAULT TRUE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE product_reviews (
+    product_review_id UUID PRIMARY KEY,
+    rating FLOAT NOT NULL,
+    comment TEXT,
+    user_id UUID NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
+    product_id UUID NOT NULL REFERENCES products(product_id) ON DELETE CASCADE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE shopping_carts (
+    shopping_cart_id UUID PRIMARY KEY,
+    session_id UUID NOT NULL,
+    user_id UUID NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE cart_items (
+    cart_item_id UUID PRIMARY KEY,
+    quantity INTEGER NOT NULL,
+    shopping_cart_id UUID NOT NULL REFERENCES shopping_carts(shopping_cart_id) ON DELETE CASCADE,
+    product_variant_id UUID NOT NULL REFERENCES product_variants(product_variant_id) ON DELETE CASCADE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE orders (
+    order_id UUID PRIMARY KEY,
+    total_amount FLOAT NOT NULL,
+    payment_status VARCHAR(20) NOT NULL,
+    payment_method VARCHAR(20) NOT NULL,
+    order_status VARCHAR(20) NOT NULL,
+    issue_date DATE NOT NULL,
+    delivered_date DATE,
+    user_id UUID NOT NULL REFERENCES user(user_id) ON DELETE CASCADE,
+    billing_address_id UUID NOT NULL REFERENCES user_addresses(billing_address_id) ON DELETE CASCADE,
+    shipping_address_id UUID NOT NULL REFERENCES user_addresses(shipping_address_id) ON DELETE CASCADE
+
+);
+
+CREATE TABLE order_items (
+    order_item_id UUID PRIMARY KEY,
+    quantity INTEGER NOT NULL,
+    price FLOAT NOT NULL,
+    order_id UUID NOT NULL REFERENCES orders(order_id) ON DELETE CASCADE,
+    product_variant_id UUID NOT NULL REFERENCES product_variants(product_variant_id) ON DELETE CASCADE
+
 );
 
 
--- 9. Inquiries
-CREATE TABLE inquiries (
-    inquiry_id BIGSERIAL PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    email VARCHAR(150) NOT NULL,
-    company VARCHAR(50),
-    job_title VARCHAR(50),
-    phone VARCHAR(20),
-    country VARCHAR(30),
-    message TEXT NOT NULL,
-    status VARCHAR(20) CHECK (status IN ('Unread','Read','Replied')) DEFAULT 'Unread',
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
--- 10. Blogs
-CREATE TABLE blogs (
-    blog_id SERIAL PRIMARY KEY,               
-    title VARCHAR(255) NOT NULL,         
-    category VARCHAR(100) NOT NULL,      
-    excerpt TEXT NOT NULL,               
-    content TEXT NOT NULL,               
-    image TEXT,                          
-    date DATE DEFAULT CURRENT_DATE, 
-    read_time VARCHAR(50) DEFAULT '5 min read', 
-    author_name VARCHAR(100) NOT NULL,   
-    author_avatar TEXT,                  
-    author_role VARCHAR(100),            
-    active BOOLEAN DEFAULT TRUE,         
-    created_at TIMESTAMP DEFAULT NOW(),  
-    updated_at TIMESTAMP DEFAULT NOW()   
-);
 
 
--- 11. Testimonials
-CREATE TABLE testimonials (
-    testimonial_id BIGSERIAL PRIMARY KEY,
-    client_name VARCHAR(100),
-    client_email VARCHAR(150),
-    imageUrl TEXT,
-    company_name VARCHAR(100),
-    designation VARCHAR(100),
-    feedback TEXT,
-    token TEXT UNIQUE,
-    active BOOLEAN DEFAULT FALSE,
-    feedback_taken BOOLEAN DEFAULT FALSE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
--- -- 12. Bookings
--- CREATE TABLE bookings (
---     booking_id BIGSERIAL PRIMARY KEY,
---     user_id VARCHAR(20) REFERENCES users(user_id) ON DELETE CASCADE,
---     service_id BIGINT REFERENCES services(service_id) ON DELETE CASCADE,
---     booking_date DATE,
---     status VARCHAR(20) CHECK (status IN ('Pending','Accepted','Completed','Cancelled')) DEFAULT 'Pending',
---     note TEXT,
---     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
--- );
-
--- 13. Pay Slips
--- Salary Payments Table
-CREATE TABLE payslips (
-    payslip_id BIGSERIAL PRIMARY KEY,
-
-    -- Company info
-    company_name VARCHAR(255) NOT NULL DEFAULT 'm³ techOps Ltd.',
-    company_address TEXT NOT NULL,
-
-    -- Reference & period
-    reference VARCHAR(255),
-    payment_month VARCHAR(20), -- e.g. "September 2025"
-
-    -- Employee info
-    employee_name VARCHAR(255) NOT NULL,
-    designation VARCHAR(255),
-    employee_id VARCHAR(50),
-
-    pay_date DATE NOT NULL,
-    earnings NUMERIC(12,2) DEFAULT 0,
-    deductions NUMERIC(12,2) DEFAULT 0,
-    net_pay NUMERIC(12,2) DEFAULT 0,
-
-    payment_mode VARCHAR(50) DEFAULT 'Bank Transfer', -- e.g., Bank / Bkash / Cash
-    account_holder VARCHAR(255),
-    bank_name VARCHAR(255),
-    bank_branch VARCHAR(255),
-    account_number VARCHAR(100),
-    bkash_transaction VARCHAR(100),
-
-    authorized_by VARCHAR(255) DEFAULT 'Sumaiya Ahmed',
-    payee VARCHAR(255),
-
-    logo TEXT,      -- store as URL or Base64
-    logo_url TEXT,
-    note TEXT,
-    status VARCHAR(20) DEFAULT 'Pending', -- e.g.  Paid / Pending / Failed
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-
--- 14. Subscribers
-CREATE TABLE subscribers (
-    subscriber_id BIGSERIAL PRIMARY KEY,
-    email VARCHAR(150),
-    status VARCHAR(10) CHECK (status IN ('Active','Inactive')) DEFAULT 'Active',
-    subscribed_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
--- 15. Newsletters
-CREATE TABLE newsletters (
-    newsletter_id BIGSERIAL PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    content TEXT NOT NULL,
-    status VARCHAR(20) CHECK (status IN ('Draft', 'Sent', 'Canceled')) DEFAULT 'Draft',
-    sent_date TIMESTAMP,      -- actual sent date
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
 
 
 CREATE TABLE policies (
@@ -261,18 +147,22 @@ VALUES
 
 
 
--- Add to your existing migrations file
--- Make sure the scheduler_slots table has the correct columns
-CREATE TABLE IF NOT EXISTS scheduler_slots (
-    id BIGSERIAL PRIMARY KEY,
-    employee_id VARCHAR(50) NOT NULL,
-    start_time TIMESTAMP WITH TIME ZONE NOT NULL,  -- Use WITH TIME ZONE for better handling
-    end_time TIMESTAMP WITH TIME ZONE NOT NULL,
-    title VARCHAR(255),
-    description TEXT,
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE discount_or_coupon_codes (
+    id SERIAL PRIMARY KEY,
+    code VARCHAR(50) NOT NULL,
+    discount_type VARCHAR(50) NOT NULL, --percentage/fixed
+    discount_value FLOAT NOT NULL,
+    min_order_value FLOAT NOT NULL,
+    expiry_date DATE NOT NULL,
+    is_active BOOLEAN NOT NULL DEFAULT TRUE
 );
 
-CREATE INDEX idx_scheduler_employee_id ON scheduler_slots(employee_id);
-CREATE INDEX idx_scheduler_time_range ON scheduler_slots(start_time, end_time);
+CREATE TABLE website_settings (
+    id SERIAL PRIMARY KEY,
+    key_name VARCHAR(100) NOT NULL,
+    value TEXT NOT NULL,
+    is_active BOOLEAN NOT NULL DEFAULT TRUE
+);
+
+
+
--- backend/src/migrations/013_seed_testimonial_data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO testimonials (client_name, client_email, imageUrl, company_name, designation, feedback, active, feedback_taken)
-VALUES
-('Alice Johnson', 'alice@example.com', NULL, 'TechCorp', 'Software Engineer', 'Great experience working with your team!', TRUE, TRUE),
-
-('Charlie Lee', 'charlie@example.com', NULL, 'InnoSoft', 'Project Manager', 'Professional and reliable service!', TRUE, TRUE),
-
-('Ethan Williams', 'ethan@example.com', NULL, 'BuildRight', 'Civil Engineer', 'Highly satisfied with the project delivery.', TRUE, TRUE);

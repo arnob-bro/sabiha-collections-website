63e398a711e7f9f099ec1b2cb223b00e42929405
arnob-bro <abdulmohaimenkhanarnob@gmail.com>
2025-11-19 00:47:05 +0600

    backend: created docs/login_signup.md

--- backend/app.js
+++ backend/app.js
@@ -3,25 +3,25 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
 const createAuthRouter = require("./src/routes/authRoutes");
-const createInquiryRouter = require("./src/routes/inquiryRoutes");
-const createRoleRouter = require("./src/routes/roleRoutes");
-const createPolicyRouter = require("./src/routes/policyRoutes");
-const createEmployeeRouter = require("./src/routes/employeeRoutes");
-const createTestimonialRouter = require("./src/routes/testimonialRoutes");
+// const createInquiryRouter = require("./src/routes/inquiryRoutes");
+// const createRoleRouter = require("./src/routes/roleRoutes");
+// const createPolicyRouter = require("./src/routes/policyRoutes");
+// const createEmployeeRouter = require("./src/routes/employeeRoutes");
+// const createTestimonialRouter = require("./src/routes/testimonialRoutes");
 
 const AuthController = require("./src/controllers/authController");
-const InquiryController = require("./src/controllers/inquiryController");
-const RoleController = require("./src/controllers/roleController");
-const PolicyController = require("./src/controllers/policyController");
-const EmployeeController = require("./src/controllers/employeeController");
-const TestimonialController = require("./src/controllers/testimonialController");
+// const InquiryController = require("./src/controllers/inquiryController");
+// const RoleController = require("./src/controllers/roleController");
+// const PolicyController = require("./src/controllers/policyController");
+// const EmployeeController = require("./src/controllers/employeeController");
+// const TestimonialController = require("./src/controllers/testimonialController");
 
 const UserService = require("./src/services/userService");
-const InquiryService = require("./src/services/inquiryService");
-const RoleService = require("./src/services/roleService");
-const PolicyService = require("./src/services/policyService");
-const EmployeeService = require("./src/services/employeeService");
-const TestimonialService = require("./src/services/testimonialService");
+// const InquiryService = require("./src/services/inquiryService");
+// const RoleService = require("./src/services/roleService");
+// const PolicyService = require("./src/services/policyService");
+// const EmployeeService = require("./src/services/employeeService");
+// const TestimonialService = require("./src/services/testimonialService");
 
 
 const db = require("./src/config/database");
@@ -33,9 +33,7 @@ const app = express();
 
 // Middlewares
 const allowedOrigins = [
-  "http://localhost:5173",
-  "https://m3techops-website.vercel.app",
-  "https://www.m3techops.com"
+  "http://localhost:5173"
 ];
 
 app.use(cors({
@@ -59,34 +57,34 @@ app.get("/", (req, res) => {
 
 //Dependency injection
 const userService = new UserService(db);
-const inquiryService = new InquiryService(db);
-const roleService = new RoleService(db);
-const policyService = new PolicyService(db);
-const employeeService = new EmployeeService(db, userService, roleService);
-const testimonialService = new TestimonialService(db);
+// const inquiryService = new InquiryService(db);
+// const roleService = new RoleService(db);
+// const policyService = new PolicyService(db);
+// const employeeService = new EmployeeService(db, userService, roleService);
+// const testimonialService = new TestimonialService(db);
 
 const authController = new AuthController(userService);
-const inquiryController = new InquiryController(inquiryService);
-const roleController = new RoleController(roleService);
-const policyController = new PolicyController(policyService);
-const employeeController = new EmployeeController(employeeService);
-const testimonialController = new TestimonialController(testimonialService);
+// const inquiryController = new InquiryController(inquiryService);
+// const roleController = new RoleController(roleService);
+// const policyController = new PolicyController(policyService);
+// const employeeController = new EmployeeController(employeeService);
+// const testimonialController = new TestimonialController(testimonialService);
 
 const authRouter = createAuthRouter(authController);
-const inquiryRouter = createInquiryRouter(inquiryController);
-const roleRouter = createRoleRouter(roleController);
-const policyRouter = createPolicyRouter(policyController);
-const employeeRouter = createEmployeeRouter(employeeController);
-const testimonialRouter = createTestimonialRouter(testimonialController);
+// const inquiryRouter = createInquiryRouter(inquiryController);
+// const roleRouter = createRoleRouter(roleController);
+// const policyRouter = createPolicyRouter(policyController);
+// const employeeRouter = createEmployeeRouter(employeeController);
+// const testimonialRouter = createTestimonialRouter(testimonialController);
 
 
 
 // API routes
 app.use("/auth", authRouter);
-app.use("/inquiry", inquiryRouter);
-app.use("/role", roleRouter);
-app.use("/policy", policyRouter);
-app.use("/employee", employeeRouter);
-app.use("/testimonial", testimonialRouter);
+// app.use("/inquiry", inquiryRouter);
+// app.use("/role", roleRouter);
+// app.use("/policy", policyRouter);
+// app.use("/employee", employeeRouter);
+// app.use("/testimonial", testimonialRouter);
 
 module.exports = app;
